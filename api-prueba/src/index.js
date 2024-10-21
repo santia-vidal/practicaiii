6f4812bb0ce8bc7069b578cf5087b32e5c2e3ba1
santia-vidal <gago5osvaldo23@gmail.com>
2024-10-21 20:38:05 -0300

    clase 21/10

--- api-prueba/src/index.js
+++ api-prueba/src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const morgan = require('morgan')
 const router = require('./routes/cliente.route')
 
+const socketio = require('socket.io')
+const osu = require('node-os-utils')
 
 //ejecutamos express para acceder a sus funcionalidades
 //dicha funcion nos retorn las herramientas necesarias
@@ -22,6 +24,19 @@ server.use(require('./routes/cliente.route'))
 server.set('port', process.env.PORT || 3002)
 
 //estamos corriendo el servidor
-server.listen( server.get('port'))
+const servidor = server.listen( server.get('port'))
 
 console.log('Servidor corriendo en el puerto: '+server.get('port'))
+
+const io = socketio(servidor)
+const cpu = osu.cpu
+
+io.on('connection', () => {
+
+    setInterval( () => {
+        cpu.usage().then( (cpuPorcentual) => {
+            io.sockets.emit('cpuPorcentual',cpuPorcentual)
+            console.log(cpuPorcentual)
+        })
+    },2000)
+})
