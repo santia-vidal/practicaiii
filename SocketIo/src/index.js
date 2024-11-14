750087f0360583eec96019ffc6054323472bd51f
santia-vidal <gago5osvaldo23@gmail.com>
2024-11-14 20:24:31 -0300

    avance en proyecto(en funcionamiento)

--- SocketIo/src/index.js
+++ SocketIo/src/index.js
@@ -1,46 +1,39 @@
+//declaro constantes
 const express = require('express')
 const cors = require('cors')
-const morgan = require('morgan')
-const router = require('./routes/cliente.route')
-const os = require('node-os-utils')
-
-//acceder a los recuros
-const cpu = os.cpu
-
 const socketio = require('socket.io')
-const osu = require('node-os-utils')
 
-//ejecutamos express para acceder a sus funcionalidades
-//dicha funcion nos retorn las herramientas necesarias
+//declaro constante
 const server = express()
 
-//especificamos la notacion JSON para el flujo de datos.
-server.use(express.json())
-//hace posible la comunicacion entre cliente y servidor
-server.use(cors())
-//esta dependencia hace posible monitorear las solicitudes http
-server.use(morgan('dev'))
-
-//pongo en funcionamiento las rutas existentes en cliente.router
-server.use(require('./routes/cliente.route'))
-
-//seteamos puerto necesario para correr el server
+//seteamos el puerto en el que va a correr el server
 server.set('port', process.env.PORT || 3002)
+server.use(cors())
 
 //estamos corriendo el servidor
-const servidor = server.listen( server.get('port'))
-
-console.log('Servidor corriendo en el puerto: '+server.get('port'))
+const servidor = server.listen( server.get('port'), () => {
+    console.log('La conexión fue exitosa!')
+});
 
 //le pasamos el servidor a socketio, para habilitar el canal websocket
 const io = socketio(servidor)
 
-io.on('connection', () => {
+//declaro variable
+var cpu = osu.cpu
+
+//establecemos la apertura del canal, par emitir posteriormente los datos
+io.on('connection', (socket) => {
 
     setInterval( () => {
-        cpu.usage().then( (cpuPorcentual) => {
-            io.sockets.emit('cpuPorcentual',cpuPorcentual)
-            console.log(cpuPorcentual)
-        })
-    },2000)
-})
+        socket.emit('dato-socket',
+            {
+                dato:Math.random(),
+                nombre:'Santi'
+            }
+        )
+    }, 1000)
+
+    socket.on('respuesta', (valor) => {
+        console.log(valor)
+    });
+});
--- vue/practica/src/components/Socketindicador.vue
+++ vue/practica/src/components/Socketindicador.vue
@@ -4,9 +4,12 @@
         <div class="row">
             <div class="col-3">
                 <div class="mb-3">
-                    <select @equipo="capturar_random()" v-model="equipo" class="form-select">
-                        <option value="-1" selected disabled>Seleccione equipo...</option>
+                    <select @change="capturar_random()" v-model="equipo" class="form-select">
+                        <option value="" selected disabled>Seleccione equipo...</option>
                         <option value="http://localhost:3002"> Mi equipo</option>
+                        <option value="htt://172.168.XXX.XXX:3000">Equipo Nacho</option>
+                        <option value="http://172.168.XXX.XXX:3000">Equipo Roberto</option>
+                        <option value="http://172.168.XXX.XXX:3000">Equipo Nico</option>
                     </select>
                 </div>
             </div>
@@ -15,12 +18,12 @@
             <div class="col-4">
                 <div class="mb-3">
                     <div class="card">
-                        <div class="card-reader text-center"><b>{{equipo}}</b></div>
+                        <div class="card-reader text-center"><b>{{nombre}}</b></div>
                         <div class="card-body text-center">
-                            <h1>{{valor}}</h1>
+                            <h1>{{valor_recibido}}</h1>
                         </div>
                         <div class="card-footer">
-                            <h4></h4>
+                            <h4>{{estado}}</h4>
                         </div>
                     </div>
                 </div>
@@ -36,24 +39,29 @@ export default{
     name:'Socketindicador',
     data() {
         return{
-            valor : null,
-            equipo: ''
+            valor_recibido:null,
+            estado:'',
+            equipo:'',
+            nombre:''
 
         }
     },
     methods: {
         capturar_random()
         {
+            //tenemos que conectarnos y/o vincular nuestro cliente con el servidor
             const socket = io(this.equipo)
 
-            socket.on('cpuPorcentual', (valor) => {
-                this.valor = valor
+            socket.on('dato-socket', (objeto) => {
+                this.valor_recibido = objeto.dato.toFixed(2);
+                this.nombre = objeto.nombre;
+                if(this.valor_recibido > 0.7)
+                {
+                    this.estado = 'Exedido'
+                }else{
+                    this.estado = 'Normal' 
+                }
             })
         }
-    },
-    mounted()
-    {
-        this.capturar_random()
     }
-    
 }</script>
